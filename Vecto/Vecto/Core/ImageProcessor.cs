78c838c9630e8192fb0eed0b197d5b40f1c9d96a
David Akopyan <dg.akopyan@gmail.com>
2025-06-12 11:47:53 +0400

    Added Image Processor to the Core

---
+++ Vecto/Vecto/Core/ImageProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vecto.Core
+{
+    internal class ImageProcessor
+    {
+        private readonly Dictionary<string ,IImageEffect> _availiableEffects;
+
+        public ImageProcessor(IEnumerable<IImageEffect> effects)
+        {
+
+            _availiableEffects = new Dictionary<string, IImageEffect>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var effect in effects)
+            {
+                if (!_availiableEffects.ContainsKey(effect.Name))
+                {
+                    _availiableEffects.Add(effect.Name, effect);
+                }
+            }
+
+        }
+
+        public void ApplyEffects(Image image, List<EffectEntity> effects)
+        {
+            foreach (var effect in effects) {
+                if (_availiableEffects.TryGetValue(effect.EffectName, out var imageEffect))
+                {
+                    imageEffect.ApplyEffect(image, effect.Parameter);
+                }
+                else
+                {
+                    throw new ArgumentException($"Effect '{effect.EffectName}' is not available.");
+                }
+            }
+        }
+    }
+}
